08bdcf9f59de15e3dcb23b7c953c69c7b52c48bb
Aaron Gore <aaron16gore@gmail.com>
2024-03-15 12:48:01 +0800

    Cleanup

--- HerdYourCats.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HerdYourCats.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
