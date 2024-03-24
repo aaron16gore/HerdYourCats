eebc934e930d3f2725133fcc846a43632f3c3086
Aztrax <aaron16gore@gmail.com>
2024-03-24 20:34:44 +0800

    Implement Loading Spinner Component

---
+++ Hyc.Web/src/components/base-component-library/LoadingSpinner.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let iconSize: number = 1;
+
+    let className: string = "";
+
+    $: className = `fa-solid fa-spin fa-spinner fa-${iconSize}x`;
+</script>
+
+<div class="icon-container">
+    <i class={className}></i>
+    {iconSize}
+</div>
+
+<style lang="css">
+</style>
